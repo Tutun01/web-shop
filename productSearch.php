2eba244640ea61159dc4adf7c12de89cacf17adc
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-08 10:44:18 +0200

    create form for product sreach

---
+++ productSearch.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search for products</title>
+</head>
+<body>
+<form method="POST" action="model/productCheck.php">
+    <input type="text" name="productSearch" placeholder="Enter product name" required >
+    <button>Product search</button>
+</form>
+</body>
+
+</html>
